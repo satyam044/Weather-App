b2b8ef6576dc4e59fd1265e9cb8add65ecda6027
satyam <mishrasatyam9890@gmail.com>
2025-08-03 14:03:51 +0530

    Weather Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/pages/weather_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherPage(),
+    );
   }
 }
